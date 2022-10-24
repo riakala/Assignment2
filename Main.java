528cb55059631c971faea2cca744b64c0535058a
Ria Kala <105014704+riakala@users.noreply.github.com>
2022-10-24 14:29:49 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,9 +3,24 @@ package Assignment2;
 public class Main {
 	public static void main(String[] args) {
 		int gameState; 
-		Title.draw();		
+		Title.draw();	
+		
+		// make nested for loop for creating 
+		Room map[][]; // create 2d room object array
+		
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				Room newRoom = new Room();
+				newRoom.setY(i);
+				newRoom.setX(j);
+				newRoom.setMyRoomsNorth = myRoomsNorth[i][j];
+				
+				map[i][j] = newRoom;
+			}
+		}
+		
+		
 	}
 	
-	// use a constructor somewhere then call it in main depending on the exit possibly(?)
 	
 }
