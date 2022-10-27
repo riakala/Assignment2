a2e2442e76632bb5c048e394089b6d7663fd95dd
Ria Kala <105014704+riakala@users.noreply.github.com>
2022-10-27 14:30:34 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,10 +3,15 @@ package Assignment2;
 public class Main {
 
 	public static void main(String[] args) {
-		int gameState; 
-		Title.draw();	
+		int gameState = 0; 
+		int numRoomsX = 8;
+		int numRoomsY = 8;
+		int currentRoomX = 0;
+		int currentRoomY = 0;;
 	
-		// Room map[][]; // create 2d room object array
+		Room map[][] = new Room[numRoomsY][numRoomsX]; // create 2d room object array
+		Room currentRoom[][] = new Room[numRoomsY][numRoomsX];
+	
 		
 		int myRoomsNorth[][] = {
 				{0, 0, 0, 0, 0, 0, 0, 0},
@@ -52,15 +57,26 @@ public class Main {
 			    {0, 1, 1, 0, 1, 1, 1, 1}
 	};
 		
+		do {
+			Title.draw();	
+			/*
+			 * draw first room with current room variables not i and j
+			 * currentroom col/row = moveRoom return value
+			 * return currentroom
+			 * draw currentroom
+			 * call moveroom
+			 */
+		
 		for (int i = 0; i < 8; i++) {
 			for (int j = 0; j < 8; j++) {
-				Room newRoom = new Room(myRoomsNorth[i][j], myRoomsEast[i][j], myRoomsSouth[i][j], myRoomsWest[i][j]);
-				
+				map[i][j] = new Room(myRoomsNorth[i][j], myRoomsEast[i][j], myRoomsSouth[i][j], myRoomsWest[i][j]);
+				currentRoom[i][j] = map[i][j]; 
 			}
 		}
 		
-	
 		
+	
+		} while (gameState == 1);
 		
 	}
 	
