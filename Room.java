6dd43591588fa67102dff8bf1420589971974236
Ria Kala <105014704+riakala@users.noreply.github.com>
2022-10-30 20:37:59 -0400

    Update Room.java

--- Room.java
+++ Room.java
@@ -1,206 +1,207 @@
-package Assignment2;
+/* Room.java
+ * Purpose: write what this class does
+ * Last edited: October 30th, 2022
+ */
+
+package assignment2;
 import java.util.Scanner;
 
 public class Room {
+	final static String ANSI_RESET = "\u001B[0m";
+    final static String ANSI_YELLOW = "\u001B[33m";
 	Scanner myObj = new Scanner(System.in);
-	int exitNorth;
-	int exitEast;
-	int exitSouth;
-	int exitWest;
-	private int x;
-	private int y;
+	protected int exitNorth;
+	protected int exitEast;
+	protected int exitSouth;
+	protected int exitWest;
+	protected int x;
+	protected int y;
 	
-	Room(int exitNorth, int exitEast, int exitSouth, int exitWest) {
+	
+	public boolean checkBossRoom() {
+		if ((this.y == 6) && (this.x == 5)) {
+			return true;
+		} 
+		return false;
+	}
+	
+	Room (int exitNorth, int exitEast, int exitSouth, int exitWest, int x, int y) {
 		this.exitNorth = exitNorth;
 		this.exitEast = exitEast;
 		this.exitSouth = exitSouth;
 		this.exitWest = exitWest;
+		this.x = x;
+		this.y = y;
+	}	
+	
+	
+	public void drawRoom() {
+		/* rooms else/if ladder
+		   true == 1, false == 0 */
+			
+		if(exitNorth == 1 && exitEast == 1 && exitSouth == 1 && exitWest == 1) {
+			System.out.println("+++++|  |+++++\r\n"
+							 + "+++++|  |+++++\r\n"
+							 + "-----   ------\r\n"
+							 + "-----   ------\r\n"
+							 + "+++++|  |+++++\r\n"
+							 + "+++++|  |+++++");
+		} else if (exitNorth == 1 && exitEast == 1 && exitSouth == 0 && exitWest == 1) {
+			System.out.println("+++++|  |+++++\r\n"
+							 + "+++++|  |+++++\r\n"
+							 + "-----   ------\r\n"
+							 + "--------------\r\n"
+							 + "++++++++++++++\r\n"
+							 + "++++++++++++++");
+		} else if (exitNorth == 1 && exitEast == 0 && exitSouth == 1 && exitWest == 1) {
+			System.out.println("+++++|  |+++++\r\n"
+							 + "+++++|  |+++++\r\n"
+							 + "-----   |+++++\r\n"
+							 + "_____   |+++++\r\n"
+							 + "+++++|  |+++++\r\n"
+							 + "+++++|  |+++++");
+		} else if (exitNorth == 0 && exitEast == 1 && exitSouth == 1 && exitWest == 1) {
+			System.out.println("++++++++++++++\r\n"
+							 + "++++++++++++++\r\n"
+							 + "--------------\r\n"
+							 + "------  ------\r\n"
+							 + "+++++|  |+++++\r\n"
+							 + "+++++|  |+++++");
+		} else if (exitNorth == 1 && exitEast == 1 && exitSouth == 1 && exitWest == 0) {
+			System.out.println("+++++|  |+++++\r\n"
+							 + "+++++|  |+++++\r\n"
+							 + "+++++|   -----\r\n"
+							 + "+++++|   -----\r\n"
+							 + "+++++|  |+++++\r\n"
+							 + "+++++|  |+++++");
+		} else if (exitNorth == 1 && exitEast == 0 && exitSouth == 0 && exitWest == 1) {
+			System.out.println("+++++|  |+++++\r\n"
+							 + "+++++|  |+++++\r\n"
+							 + "-----   |+++++\r\n"
+							 + "________|+++++\r\n"
+							 + "++++++++++++++\r\n"
+							 + "++++++++++++++");
+		} else if (exitNorth == 1 && exitEast == 1 && exitSouth == 0 && exitWest == 0) {
+			System.out.println("+++++|  |+++++\r\n"
+							 + "+++++|  |+++++\r\n"
+							 + "+++++|   -----\r\n"
+							 + "+++++|________\r\n"
+							 + "++++++++++++++\r\n"
+							 + "++++++++++++++");
+		} else if (exitNorth == 0 && exitEast == 0 && exitSouth == 1 && exitWest == 1) {
+			System.out.println("++++++++++++++\r\n"
+							 + "++++++++++++++\r\n"
+							 + "--------|+++++\r\n"
+							 + "------  |+++++\r\n"
+							 + "+++++|  |+++++\r\n"
+							 + "+++++|  |+++++");
+		} else if (exitNorth == 0 && exitEast == 1 && exitSouth == 1 && exitWest == 0) {
+			System.out.println("++++++++++++++\r\n"
+							 + "++++++++++++++\r\n"
+							 + "+++++|--------\r\n"
+							 + "+++++|   -----\r\n"
+							 + "+++++|  |+++++\r\n"
+							 + "+++++|  |+++++");
+		} else if (exitNorth == 1 && exitEast == 0 && exitSouth == 1 && exitWest == 0) {
+			System.out.println("+++++|  |+++++\r\n"
+							 + "+++++|  |+++++\r\n"
+							 + "+++++|  |+++++\r\n"
+							 + "+++++|  |+++++\r\n"
+							 + "+++++|  |+++++\r\n"
+							 + "+++++|  |+++++");
+		} else if (exitNorth == 0 && exitEast == 1 && exitSouth == 0 && exitWest == 1) {
+			System.out.println("++++++++++++++\r\n"
+							 + "++++++++++++++\r\n"
+							 + "--------------\r\n"
+							 + "______________\r\n"
+							 + "++++++++++++++\r\n"
+							 + "++++++++++++++");
+		} else if (exitNorth == 1 && exitEast == 0 && exitSouth == 0 && exitWest == 0) {
+			System.out.println("+++++|  |+++++\r\n"
+							 + "+++++|  |+++++\r\n"
+							 + "+++++|  |+++++\r\n"
+							 + "+++++|__|+++++\r\n"
+							 + "++++++++++++++\r\n"
+							 + "++++++++++++++");
+		} else if (exitNorth == 0 && exitEast == 1 && exitSouth == 0 && exitWest == 0) {
+			System.out.println("++++++++++++++\r\n"
+							 + "++++++++++++++\r\n"
+							 + "++++++--------\r\n"
+							 + "++++++|_______\r\n"
+							 + "++++++++++++++\r\n"
+							 + "++++++++++++++");
+		} else if (exitNorth == 0 && exitEast == 0 && exitSouth == 1 && exitWest == 0) {
+			System.out.println("++++++++++++++\r\n"
+							 + "++++++++++++++\r\n"
+							 + "+++++----+++++\r\n"
+							 + "+++++|  |+++++\r\n"
+							 + "+++++|  |+++++\r\n"
+							 + "+++++|  |+++++");
+		} else if (exitNorth == 0 && exitEast == 0 && exitSouth == 0 && exitWest == 1) {
+			System.out.println("++++++++++++++\r\n"
+							 + "++++++++++++++\r\n"
+							 + "--------|+++++\r\n"
+							 + "________|+++++\r\n"
+							 + "++++++++++++++\r\n"
+							 + "++++++++++++++");
+		} else {
+			System.out.println(ANSI_YELLOW + "    (\r\n"
+					+ "     )\r\n"
+					+ "    ()\r\n"
+					+ "   |--|\r\n"
+					+ "   |  |\r\n"
+					+ " .-|  |-.\r\n"
+					+ ":  |  |  :\r\n"
+					+ ":  '--'  :\r\n"
+					+ " '-....-'\n" + ANSI_RESET + "++++++++++++++\r\n"
+							 + "++++------++++\r\n"
+							 + "+++|      |+++\r\n"
+							 + "+++|      |+++\r\n"
+							 + "+++|______|+++\r\n"
+							 + "++++++++++++++" + "    (\r\n"
+							 		+ ANSI_YELLOW + "     )\r\n\n"
+							 		+ "    ()\r\n"
+							 		+ "   |--|\r\n"
+							 		+ "   |  |\r\n"
+							 		+ " .-|  |-.\r\n"
+							 		+ ":  |  |  :\r\n"
+							 		+ ":  '--'  :\r\n"
+							 		+ " '-....-'");
+		}					
+	}
+				
+		
+	public String chooseExit() {
+		// prompts user to move around map
+		String direc;
+			
+		System.out.println("Where would you like to move? (n, e, s, w)");
+		direc = myObj.nextLine(); 
+		return direc;
 	}
 	
-	// array of rooms + general idea of 64 square map (not in use)
 	
-	int myRooms[][] = {
-			{0, 1, 2, 3, 4, 5, 6, 7},
-			{8, 9, 10, 11, 12, 13, 14, 15},
-			{16, 17, 18, 19, 20, 21, 22, 23},
-			{24, 25, 26, 27, 28, 29, 30, 31},
-			{32, 33, 34, 35, 36, 27, 38, 39},
-			{40, 41, 42, 43, 44, 45, 46, 47},
-			{48, 49, 50, 51, 52, 53, 54, 55},
-			{56, 57, 58, 59, 60, 61, 62, 63}						
-		  };
-	
-
-	
-	/* test rooms else/if ladder
-	   true == 1, false == 0 */
-	
-		public void drawRoom() {
+	public Room moveRoom(Room[][] map) {
+		// takes return value of direc and adds it to the room
+		String chooseExitReturn = chooseExit();	
 			
-			if(exitNorth == 1 && exitEast == 1 && exitSouth == 1 && exitWest == 1) {
-				System.out.println("+++++|  |+++++\r\n"
-								 + "+++++|  |+++++\r\n"
-								 + "-----   ------\r\n"
-								 + "-----   ------\r\n"
-								 + "+++++|  |+++++\r\n"
-								 + "+++++|  |+++++");
-			} else if(exitNorth == 1 && exitEast == 1 && exitSouth == 0 && exitWest == 1) {
-				System.out.println("+++++|  |+++++\r\n"
-								 + "+++++|  |+++++\r\n"
-								 + "-----   ------\r\n"
-								 + "--------------\r\n"
-								 + "++++++++++++++\r\n"
-								 + "++++++++++++++");
-			} else if (exitNorth == 1 && exitEast == 0 && exitSouth == 1 && exitWest == 1) {
-				System.out.println("+++++|  |+++++\r\n"
-								 + "+++++|  |+++++\r\n"
-								 + "-----   |+++++\r\n"
-								 + "_____   |+++++\r\n"
-								 + "+++++|  |+++++\r\n"
-								 + "+++++|  |+++++");
-			} else if(exitNorth == 0 && exitEast == 1 && exitSouth == 1 && exitWest == 1) {
-				System.out.println("++++++++++++++\r\n"
-								 + "++++++++++++++\r\n"
-								 + "--------------\r\n"
-								 + "------  ------\r\n"
-								 + "+++++|  |+++++\r\n"
-								 + "+++++|  |+++++");
-			} else if(exitNorth == 1 && exitEast == 1 && exitSouth == 1 && exitWest == 0) {
-				System.out.println("+++++|  |+++++\r\n"
-								 + "+++++|  |+++++\r\n"
-								 + "+++++|   -----\r\n"
-								 + "+++++|   -----\r\n"
-								 + "+++++|  |+++++\r\n"
-								 + "+++++|  |+++++");
-			} else if(exitNorth == 1 && exitEast == 0 && exitSouth == 0 && exitWest == 1) {
-				System.out.println("+++++|  |+++++\r\n"
-								 + "+++++|  |+++++\r\n"
-								 + "-----   |+++++\r\n"
-								 + "________|+++++\r\n"
-								 + "++++++++++++++\r\n"
-								 + "++++++++++++++");
-			} else if(exitNorth == 1 && exitEast == 1 && exitSouth == 0 && exitWest == 0) {
-				System.out.println("+++++|  |+++++\r\n"
-								 + "+++++|  |+++++\r\n"
-								 + "+++++|   -----\r\n"
-								 + "+++++|________\r\n"
-								 + "++++++++++++++\r\n"
-								 + "++++++++++++++");
-			} else if(exitNorth == 0 && exitEast == 0 && exitSouth == 1 && exitWest == 1) {
-				System.out.println("++++++++++++++\r\n"
-								 + "++++++++++++++\r\n"
-								 + "--------|+++++\r\n"
-								 + "------  |+++++\r\n"
-								 + "+++++|  |+++++\r\n"
-								 + "+++++|  |+++++");
-			} else if(exitNorth == 0 && exitEast == 1 && exitSouth == 1 && exitWest == 0) {
-				System.out.println("++++++++++++++\r\n"
-								 + "++++++++++++++\r\n"
-								 + "+++++|--------\r\n"
-								 + "+++++|   -----\r\n"
-								 + "+++++|  |+++++\r\n"
-								 + "+++++|  |+++++");
-			} else if(exitNorth == 1 && exitEast == 0 && exitSouth == 1 && exitWest == 0) {
-				System.out.println("+++++|  |+++++\r\n"
-								 + "+++++|  |+++++\r\n"
-								 + "+++++|  |+++++\r\n"
-								 + "+++++|  |+++++\r\n"
-								 + "+++++|  |+++++\r\n"
-								 + "+++++|  |+++++");
-			} else if(exitNorth == 0 && exitEast == 1 && exitSouth == 0 && exitWest == 1) {
-				System.out.println("++++++++++++++\r\n"
-								 + "++++++++++++++\r\n"
-								 + "--------------\r\n"
-								 + "______________\r\n"
-								 + "++++++++++++++\r\n"
-								 + "++++++++++++++");
-			} else if (exitNorth == 1 && exitEast == 0 && exitSouth == 0 && exitWest == 0) {
-				System.out.println("+++++|  |+++++\r\n"
-								 + "+++++|  |+++++\r\n"
-								 + "+++++|  |+++++\r\n"
-								 + "+++++|__|+++++\r\n"
-								 + "++++++++++++++\r\n"
-								 + "++++++++++++++");
-			} else if(exitNorth == 0 && exitEast == 1 && exitSouth == 0 && exitWest == 0) {
-				System.out.println("++++++++++++++\r\n"
-								 + "++++++++++++++\r\n"
-								 + "++++++--------\r\n"
-								 + "++++++|_______\r\n"
-								 + "++++++++++++++\r\n"
-								 + "++++++++++++++");
-			} else if(exitNorth == 0 && exitEast == 0 && exitSouth == 1 && exitWest == 0) {
-				System.out.println("++++++++++++++\r\n"
-								 + "++++++++++++++\r\n"
-								 + "+++++----+++++\r\n"
-								 + "+++++|  |+++++\r\n"
-								 + "+++++|  |+++++\r\n"
-								 + "+++++|  |+++++");
-			} else if(exitNorth == 0 && exitEast == 0 && exitSouth == 0 && exitWest == 1) {
-				System.out.println("++++++++++++++\r\n"
-								 + "++++++++++++++\r\n"
-								 + "--------|+++++\r\n"
-								 + "________|+++++\r\n"
-								 + "++++++++++++++\r\n"
-								 + "++++++++++++++");
-			} else {
-				System.out.println("++++++++++++++\r\n"
-								 + "++++------++++\r\n"
-								 + "+++|      |+++\r\n"
-								 + "+++|      |+++\r\n"
-								 + "+++|______|+++\r\n"
-								 + "++++++++++++++");
-			}
-					
-		}
-		
-		
-		// prompts user to move around map
+		if (chooseExitReturn.equals("n") && this.exitNorth == 1) {				
+			return map[this.y-1][this.x]; 
+			
+		} else if (chooseExitReturn.equals("e") && this.exitEast == 1) {
+			return map[this.y][this.x+1];
+			
+		} else if (chooseExitReturn.equals("s") && this.exitSouth == 1) {
+			return map[this.y+1][this.x];
+			
+		} else if (chooseExitReturn.equals("w") && this.exitWest == 1) {
+			return map[this.y][this.x-1];
+			
+		} else {
+			System.out.println("You cannot exit this direction.");
+			return this.moveRoom(map);
+			
+		}						
+	}	
 	
-		
-		public int moveRoom() {
-
-			String direc;
-			int returnValue = 0;
-			
-			System.out.println("Where would you like to move? (n, e, s, w)");
-			direc = myObj.nextLine(); 
-			
-			// takes return value of direc and adds it to the room
-			
-			if (direc.equals("n")) {				
-				if(this.exitNorth == 1) {
-					returnValue = 1;
-				} else {
-					returnValue = 0;
-					System.out.println("You cannot exit this direction.");
-				}
-				
-			} else if (direc.equals("e")) {
-				if(this.exitEast == 1) {
-					returnValue = 1;
-				} else {
-					returnValue = 0;
-					System.out.println("You cannot exit this direction.");
-				}
-				
-			} else if (direc.equals("s")) {
-				if (this.exitSouth == 1) {
-					returnValue = 1;
-				} else {
-					returnValue = 0;
-					System.out.println("You cannot exit this direction.");
-				}		
-				
-			} else if (direc.equals("w")) {
-				if (this.exitWest == 1) {
-					returnValue = 1;
-				} else {
-					returnValue = 0;
-					System.out.println("You cannot exit this direction.");
-				}			
-			}			
-			return returnValue;			
-		}
-		
-		
 }
