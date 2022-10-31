f87c21f81144db45035d6c4750649f1e449b5b33
Ria Kala <105014704+riakala@users.noreply.github.com>
2022-10-30 20:36:04 -0400

    Update Title.java

--- Title.java
+++ Title.java
@@ -1,10 +1,22 @@
+/* Title.java
+ * Purpose: Show a fun colourful display title at the start of the game, 
+ * which prompts the user to begin playing
+ * Last edited: October 30th, 2022
+ */
+
 package assignment2;
 
 public class Title {
+	// ANSI colour codes
+	final static String ANSI_RESET = "\u001B[0m";
+    final static String ANSI_YELLOW = "\u001B[33m";
+    final static String ANSI_CYAN = "\u001B[36m";
+    final static String ANSI_GREEN = "\u001B[32m";
 
 	public static void draw() {
-		System.out.println("===============================================================================");
-		System.out.println(" ________  ___  ________          ________  ________  _____ ______   _______      \r\n"
+	// Draws the title
+		System.out.println(ANSI_YELLOW + "===============================================================================");
+		System.out.println(ANSI_CYAN  + " ________  ___  ________          ________  ________  _____ ______   _______      \r\n"
 				+ "|\\   __  \\|\\  \\|\\   __  \\        |\\   ____\\|\\   __  \\|\\   _ \\  _   \\|\\  ___ \\     \r\n"
 				+ "\\ \\  \\|\\  \\ \\  \\ \\  \\|\\  \\       \\ \\  \\___|\\ \\  \\|\\  \\ \\  \\\\\\__\\ \\  \\ \\   __/|    \r\n"
 				+ " \\ \\   _  _\\ \\  \\ \\   __  \\       \\ \\  \\  __\\ \\   __  \\ \\  \\\\|__| \\  \\ \\  \\_|/__  \r\n"
@@ -12,7 +24,7 @@ public class Title {
 				+ "   \\ \\__\\\\ _\\\\ \\__\\ \\__\\ \\__\\       \\ \\_______\\ \\__\\ \\__\\ \\__\\    \\ \\__\\ \\_______\\\r\n"
 				+ "    \\|__|\\|__|\\|__|\\|__|\\|__|        \\|_______|\\|__|\\|__|\\|__|     \\|__|\\|_______|\r\n"
 				+ "                                                                                  ");
-		System.out.println("===============================================================================");
-		System.out.println("============================ PRESS ENTER TO START =============================");
+		System.out.println(ANSI_YELLOW + "===============================================================================");
+		System.out.println(ANSI_RESET + "============================= ENTER '1' TO START ==============================");
 	}
 }
